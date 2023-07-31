63ef29c03ae0223a7feda1b3add20eac1702bd65
aidyel <aidayeli15@gmail.com>
2023-07-31 10:29:15 -0400

    updated the about fade in but still need to fix it

--- script.js
+++ script.js
@@ -1,60 +1,59 @@
+// Function to handle the fade-in effect
 //  slide in effect
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        console.log("each element: ", entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-            observer.unobserve(entry.target); // Stop observing after it’s visible
-        } 
+            entry.target.classList.remove('hidden');
+
+        } else  {
+            entry.target.classList.remove('show');
+            entry.target.classList.add('hidden');
+        }
+        return true
+        
     })
 });
 
-const hiddenElements = document.querySelectorAll('#hidden')
+const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el))
 
 
+
 document.getElementById('btnSwitch').addEventListener('click', () => {
-    document.body.classList.toggle("dark-theme")
-    document.body.classList.toggle("bg-dark")
-    document.body.classList.toggle("text-white")
-
-})
-
-// validate form
+    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("bg-dark");
+    document.body.classList.toggle("text-white");
+});
 
 function validate() {
-    if (document.myForm.Name.value === "") {
+    if (document.myForm.Name.value == "") {
         alert("Please provide your name!");
         document.myForm.Name.focus();
         return false;
     }
-    if (document.myForm.Subject.value === "") {
+    if (document.myForm.Subject.value == "") {
         alert("Please provide a subject!");
         document.myForm.Subject.focus();
         return false;
     }
-    if (document.myForm.Email.value === "") {
+    if (document.myForm.Email.value == "") {
         alert("Please provide your Email!");
-        document.myForm.Email.focus();
+        document.myForm.EMail.focus();
         return false;
     }
-    if (document.myForm.Phone.value === "" || isNaN(document.myForm.Phone.value) || document.myForm.Phone.value.length !== 10) {
-        alert("Please provide a valid 10-digit phone number!");
+    if (document.myForm.Phone.value == "" || isNaN(document.myForm.Phone.value) ||
+        document.myForm.Phone.value.length != 5) {
+        alert("Please Provide Your Phone Number");
         document.myForm.Phone.focus();
         return false;
     }
-    if (document.myForm.Message.value === "") {
+    if (document.myForm.Message.value == "") {
         alert("Please Enter Your Message!");
         document.myForm.Message.focus();
         return false;
     }
     return true;
 }
-
-
-
-
-
-
-

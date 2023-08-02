15c84e6b03b7f1bd1d329b6ea12005b064239bca
aidyel <aidayeli15@gmail.com>
2023-08-02 15:17:50 -0400

    refactor the validation form to jQuery

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
 // Function to handle the fade-in effect
 //  slide in effect
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log("each element: ", entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-            entry.target.classList.remove('slide-in');
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log("each element: ", entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//             entry.target.classList.remove('slide-in');
 
-        } else  {
-            entry.target.classList.remove('show');
-            entry.target.classList.add('slide-in');
-        }
-        return true
+//         } else  {
+//             entry.target.classList.remove('show');
+//             entry.target.classList.add('slide-in');
+//         }
+//         return true
         
-    })
-});
+//     })
+// });
 
-const hiddenElements = document.querySelectorAll('.slide-in')
-hiddenElements.forEach((el) => observer.observe(el))
+// const hiddenElements = document.querySelectorAll('.slide-in')
+// hiddenElements.forEach((el) => observer.observe(el))
 
 
+
+// form validation jQuery
+// Wait for the DOM to be ready
+$(function() {
+    // Initialize form validation on the form.
+    // It has the name attribute "myForm"
+    $("form[name='myForm']").validate({
+      // Specify validation rules
+      rules: {
+        // The key name on the left side is the name attribute
+        // of an input field. Validation rules are defined
+        // on the right side
+        name: "required",
+        subject: "required",
+        email: {
+          required: true,
+          // Specify that email should be validated
+          // by the built-in "email" rule
+          email: true
+        },
+        phone: {
+            required: true,
+            phoneUs: true
+        },
+        message:"required",
+
+      },
+      // Specify validation error messages
+      messages: {
+        name: "Please enter your name",
+        email: "Please enter a valid email address",
+        subject: "Please provide a subject!",
+        phone: "please provide a phone number",
+        message: "Please enter your message",
+      },
+      // Make sure the form is submitted to the destination defined
+      // in the "action" attribute of the form when valid
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
 
 document.getElementById('btnSwitch').addEventListener('click', () => {
     document.body.classList.toggle("dark-theme");
     document.body.classList.toggle("bg-dark");
     document.body.classList.toggle("text-white");
 });
-
-function validate() {
-    if (document.myForm.Name.value == "") {
-        alert("Please provide your name!");
-        document.myForm.Name.focus();
-        return false;
-    }
-    if (document.myForm.Subject.value == "") {
-        alert("Please provide a subject!");
-        document.myForm.Subject.focus();
-        return false;
-    }
-    if (document.myForm.Email.value == "") {
-        alert("Please provide your Email!");
-        document.myForm.EMail.focus();
-        return false;
-    }
-    if (document.myForm.Phone.value == "" || isNaN(document.myForm.Phone.value) ||
-        document.myForm.Phone.value.length != 5) {
-        alert("Please Provide Your Phone Number");
-        document.myForm.Phone.focus();
-        return false;
-    }
-    if (document.myForm.Message.value == "") {
-        alert("Please Enter Your Message!");
-        document.myForm.Message.focus();
-        return false;
-    }
-    return true;
-}

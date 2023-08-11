815457f5ec76a3532441c1df1b183ff87db1c901
aidyel <aidayeli15@gmail.com>
2023-08-11 11:37:47 -0400

    added sorted list folder

--- sorted-list/sorted-list.js
+++ sorted-list/sorted-list.js
@@ -1,68 +1,74 @@
 const list = [
     "HTML 5",
     "CSS3",
-    "JavaScript",
-    "React",
     "Angular 2",
+    "React",
     "Node.js",
-    "Bootstrap"
-];
+    "Vue.js",
+    "PHP",
+    "JavaScript",
+    "Bootstrap",
+  ];
+  
+  // Get Elements from DOM and store them in constants
+  
+  const output = document.querySelector(".output");
+  const search = document.querySelector(".filter-input");
+  const sortBtn = document.querySelector('.sort-btn');
+  const icon = document.getElementById('icon');
 
-// Get elements from DOM & store them in constants  
-const output = document.querySelector(".output");
-const search = document.querySelector('.filter-input');
-const sortBtn = document.querySelector('.sort-btn');
-const icon = document.getElementById('icon');
 
-
-// Listen to user Event
+// Listen to User Event
 window.addEventListener("DOMContentLoaded", loadList);
 search.addEventListener("input", filter);
-sortBtn.addEventListener("click", sortList)
+sortBtn.addEventListener("click", sortList);
 
-// Global Variable
-let ascendingSort = false;
+// Global Vriables
+let AssendingSort = false; 
 let result;
 
-
-// loading function
-function loadList(){
+// Loading Function
+function loadList() {
     let temp = `<ul class="list-items">`;
     list.forEach((item) => {
-        temp += `<li class="list-item">${item}</li>`
-    })
+      temp += `<li class="list-item"> ${item} </li>`;
+    });
     temp += `</ul>`;
     output.innerHTML = temp;
-};
+  }
 
 // filter function
 function filter(e) {
-    result = list.filter(item => item.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()));
+
+    result  = list.filter(item=> item.toLowerCase().includes(e.target.value.toLowerCase()));
     let temp = `<ul class="list-items">`;
-
-    if(result.length <= 0) {
-        result=['No Item Found'];
+   
+    if(result.length<=0){
+      result=['No Item Found'];
     }
-    result.forEach(item => {
-        if (item === "No Item Found") {
-            temp+=`<li class="no-item">${item}</li>`;
-        } else {
-            temp+=`<li class="list-item">${item}</li>`;
-        }
+    result.forEach(item=>{
+      if(item === 'No Item Found'){
+        temp+=`<li class="no-item"> ${item}</li>`;
+      }else{
+        temp+=`<li class="list-item"> ${item}</li>`;
+      }
     })
+    
+    temp+=`</ul>`
 
-    temp+=`</ul>`;
-    output.innerHTML = temp;
+    output.innerHTML =temp;
 }
 
+
 // sorting function
-function sortList() {
-    let temp = "";
+function sortList(){
+    let temp="";
     let sortedList;
-
-    ascendingSort = !ascendingSort;
-
-    if (ascendingSort){
+  
+    AssendingSort = !AssendingSort;
+  
+    if(AssendingSort){
+  
         icon.classList.remove("fa-sort-down");
         icon.classList.add("fa-sort-up");
 
@@ -73,18 +79,16 @@ function sortList() {
         icon.classList.add("fa-sort-down");
 
         sortedList = result.sort().reverse();
-
     }
-
     temp = `<ul class="list-items">`;
     sortedList.forEach((item) => {
-        if (item === "No Item Found") {
-            temp+=`<li class="no-item">${item}</li>`;
-        } else {
-            temp+=`<li class="list-item">${item}</li>`;
-        }
-    })
+      if(item === 'No Item Found'){
+        temp+=`<li class="no-item"> ${item}</li>`;
+      }else{
+        temp+=`<li class="list-item"> ${item}</li>`;
+      }
+    });
     temp += `</ul>`;
     output.innerHTML = temp;
-
+  
 }
